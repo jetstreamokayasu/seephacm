edfbfea36c744ddc13acaf0c2c9ac1963851a393
jetstreamokayasu <tokotsuku2@outlook.jp>
2019-03-01 21:00:58 +0900

    add functions to analyze result by "phacm"

--- R/compute_bettis.R
+++ R/compute_bettis.R
@@ -1,13 +1,13 @@
 #' Calculating Betti numbers of list of point cloud data
-#' 
+#'
 #' @param X list of point cloud data
 #' @param maxdim the max dimension to compute persistent homology
 #' @param maxscale the maximum of range to compute
 #' @param samples the number of subsamples
-#' @param const.size the number of points in one subsamples
+#' @param const.size the number of points in one subsamples.
 #'   when 'const.size=F', the number of points in one subsamples is the 80% number of points in original data
 #' @export
-#' @return aggrs list that have 1st and 2nd Betti numbers, the number of data, the number of points in one subsamples
+#' @return return list that have 1st and 2nd Betti numbers, the number of data, the number of points in one subsamples
 #'   the number of subsamples, max dimension, and max scale
 
 calc_bettis <- function(X, maxdim, maxscale, samples, const.size=F){
@@ -15,27 +15,27 @@ calc_bettis <- function(X, maxdim, maxscale, samples, const.size=F){
   aggr2 <- matrix(0,length(X),1)
   rownames(aggr1) <- paste0("data-set", 1:length(X))
   rownames(aggr2) <- rownames(aggr1)
-  
+
   for(t in 1:length(X)){
-    
+
     cat("data set", t, "calculating\n")
     if(const.size==F){size<-nrow(X[[t]])*(4/5)}
     else{size<-const.size}
-    
+
     B <- bootstrapper(X[[t]], size, samples)
     speak <- bootstrap_homology(B,maxdim,maxscale)
-    
+
     aggr1[t,1] <- mean(speak[1,])
     aggr2[t,1] <- mean(speak[2,])
   }
-  
+
   aggrs <- list(aggr1,aggr2)
- 
+
   aggrs <- append(aggrs,list(Xsize=sapply(1:length(X), function(l)nrow(X[[l]])), Xsamples=length(X),
                              Bsize=size,Bsamples=samples,
                              maxdim=maxdim, maxscale=maxscale))
   class(aggrs) <- "bettiComp"
-  
+
   return(aggrs)
 }
 
@@ -44,7 +44,7 @@ calc_bettis <- function(X, maxdim, maxscale, samples, const.size=F){
 #' @return "bootsSamples" class
 
 bootstrapper <- function(X,size,samples){
-  
+
   hausdint <- TDA::hausdInterval(X = X,m = size,B = samples)
   X <- lapply(1:samples,function(i)X[sample(nrow(X),size),])
   attr(X,"size") <- size
@@ -59,7 +59,7 @@ bootstrapper <- function(X,size,samples){
 #' @importFrom phacm compute_pd
 #' @importFrom phacm compute_pl
 #' @importFrom phacm count_smooth_maximal
-#' @importFrom magrittr %>% 
+#' @importFrom magrittr %>%
 
 bootstrap_homology <- function(X,maxdim,maxscale,const.band=0,maximum.thresh = F){
 
@@ -67,17 +67,17 @@ bootstrap_homology <- function(X,maxdim,maxscale,const.band=0,maximum.thresh = F
   peak <- matrix(0,maxdim,length(X))
 
   diags <- lapply(X,function(x)phacm::compute_pd(x,maxdim,maxscale))
-  
+
   for (t in 1:length(X)) {
     pd<-diags[[t]]
     attr(pd, "maxdimension")<-maxdim
     attr(pd, "scale")<-c(0, maxscale)
     land <- phacm::compute_pl(pd)
     if(maximum.thresh) band <- max(sapply(land,max))/4
-    peak[, t]<- phacm::count_smooth_maximal(land, exist.method = per_mean, cutoff.method = per_mean)[, "betti"] %>% 
+    peak[, t]<- phacm::count_smooth_maximal(land, exist.method = per_mean, cutoff.method = per_mean)[, "betti"] %>%
       as.matrix()
   }
-  
+
   dimnames(peak) <- list(paste0("dim",1:maxdim),paste0("sample",1:length(X)))
 
   return(peak)
@@ -88,9 +88,147 @@ bootstrap_homology <- function(X,maxdim,maxscale,const.band=0,maximum.thresh = F
 #' @importFrom phacm zero_hat_double_threshold
 
 per_mean<-function(pd){
-  
+
   mean<-phacm::zero_hat_double_threshold(pd)
-  
+
   return(mean/2)
-  
+
 }
+
+
+#' Calculating persistence of n-dimensional holes
+#' @param pd persisitence diagram by 'phacm'
+#' @param n dimension
+#' @importFrom phacm persistence
+#' @export
+calcper<-function(pd, n){
+
+  per<-phacm::persistence(pd)
+  per.dim<-per[per[,"dim"]==n, "persistence"] %>% as.vector()
+
+  if(n==0){per.dim<-per.dim[-1, ]}
+
+  return(per.dim)
+}
+
+
+#' Counting the number of data sets estimated that have n holes
+#' @param aggr estimated result using 'calc_bettis'
+#' @param dim dimension of holes
+#' @export
+
+cycle_number<-function(aggr, dim){
+
+  cyc.num<-max(aggr[[dim]])
+
+  if(cyc.num > 4){col<-cyc.num}
+  else{col<-4}
+
+  estimate<-matrix(0, ncol(aggr[[dim]]), col+1)
+  colnames(estimate)<-c(paste0(0:col, "hole"))
+
+  for (i in 1:(col+1)) {
+    for (j in 1:ncol(aggr[[dim]])) {
+      estimate[j, i]<-meetNumber(aggr[[dim]][, j], i-1)
+    }
+  }
+
+  return(estimate)
+
+}
+
+
+#' Counting correct result
+#' @param result estimated Betti numbers
+#' @param correct correct Betti numbers
+
+meetNumber<-function(result, correct){
+
+  return(length(result[result >= (correct-0.5) & result < (correct+0.5)]))
+
+}
+
+#' Calculating succsess rate of results
+#' @param aggrlist list of estimate result of 'calc_bettis'
+#' @param correct vector of correct Betti numbers
+
+aggr_success_rates<-function(aggrlist, correct){
+
+  rates<-lapply(aggr.list, function(aggr){
+
+    set.sum<-length(aggr[[1]])
+
+    cycle1<-cycle_number(aggr[[1]], compare=F)
+    rate1<-cycle1[correct[1]+1]/set.sum
+
+    cycle2<-cycle_number(aggr[[2]], compare=F)
+    rate2<-cycle2[correct[2]+1]/set.sum
+
+    return(list(dim1rate=rate1, dim2rate=rate2))
+
+  })
+
+
+  return(rates)
+
+}
+
+#' Calculating persistence landscape from persistence diagram by 'TDA' and ploting
+#' @param diag persistence diagram
+#' @param maxscale the maximum of range to compute
+#' @importFrom TDA landscape
+#' @export
+
+calc_landscape<-function(diag, maxscale, line=T){
+
+  thresh<-phacm::calcDiagCentroid(diag)[3]
+  tseq <- seq(0, maxscale, length = 1000)
+  Land.dim1 <- TDA::landscape(diag[[1]], dimension = 1, KK = 1, tseq)
+  plot(tseq, Land.dim1, type = "l", col=2, xlab = "(Birth + Death) / 2",ylab = "(Death - Birth) / 2", ylim=c(0, round(max(Land.dim1)+1)/2), main ="1-degree landscape")
+  if(line){
+    abline(h=thresh)
+  }
+
+  if(length(diag[[1]][diag[[1]][,1]==2,])>0){
+    Land.dim2 <- TDA::landscape(diag[[1]], dimension = 2, KK = 1, tseq)
+    plot(tseq, Land.dim2, type = "l", col=3, xlab = "(Birth + Death) / 2",ylab = "(Death - Birth) / 2", ylim=c(0, round(max(Land.dim2)+1)/2), main ="2-degree landscape")
+    if(line){
+      abline(h=thresh/2)
+    }
+
+    return(list(tseq=tseq, Land.dim1=Land.dim1, Land.dim2=Land.dim2, thresh=thresh))
+
+  }else{return(list(tseq=tseq, Land.dim1=Land.dim1, thresh=thresh))}
+}
+
+
+#' Plotingpersistence  landscape
+#' @param land persistence landscape calculated by 'calc_landscape'
+#' @export
+
+plotLandscape<-function(land){
+
+  plotland<-lapply(2:(length(land)-1), function(k){
+
+    if(names(land)[k]=="Land.dim1"){
+
+      plot(land[[1]], land[[k]], type = "l", col=k, xlab = "(Birth + Death) / 2", ylab = "(Death - Birth) / 2", ylim=c(0, round(max(land[[k]])+1)/2), main =paste0(k-1, "-degree landscape"))
+      abline(h=land[["thresh"]])
+
+    }
+
+    else if(names(land)[k]=="Land.dim2"){
+
+      plot(land[[1]], land[[k]], type = "l", col=3, xlab = "(Birth + Death) / 2",ylab = "(Death - Birth) / 2", ylim=c(0, round(max(land[[k]])+1)/2), main =paste0(2, "-degree landscape"))
+      abline(h=land[["thresh"]]/2)
+
+    }else{
+
+      plot(land[[1]], land[[k]], type = "l", col=k, xlab = "(Birth + Death) / 2",ylab = "(Death - Birth) / 2", ylim=c(0, round(max(land[[k]])+1)/2), main =paste0(k-1, "-degree landscape"))
+
+    }
+
+  })
+
+}
+
---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Generating points distributing on disk uniformly
+#' @param N the number of points
+#' @param R radius
+
+uniDisMake<-function(N, R){
+
+  theta <- runif(N, min=0, max=2*pi)
+  r <- sqrt(2*runif(N, min=0, max=0.5*R^2))
+  df <- data.frame(x=r*cos(theta), y=r*sin(theta))
+  return(df)
+
+}
+
+
+#' Combing 'plot3d' and 'aspect3d' to plot 3d data
+#' @param X 3d data
+#' @importFrom rgl plot3d
+#' @importFrom rgl aspect3d
+
+figurePlot3d<-function(X){
+
+  rgl::plot3d(X)
+  rgl::aspect3d("iso")
+
+}
+
+#' Saving variable into RData file
+#' @param ... one variable
+#' @importFrom magrittr %>%
+
+save2Rdata <- function(...) {
+
+  elp <- list(...)
+  elname <- substitute(...) %>% as.character()
+  assign(elname, elp[[1]])
+  save(list = elname, file = paste0("./data/", gsub("\\.", "_", elname), ".RData"))
+}
